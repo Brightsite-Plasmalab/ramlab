2aafa617238f1355014e31200bf1efa9452e00e1
Aleksandr Pikalev <aleksandr.pikalev@maastrichtuniversity.nl>
2025-09-23 10:46:01 +0200

    Remove deleted linelist unit

--- src/ramlab/__init__.py
+++ src/ramlab/__init__.py
@@ -1,7 +1,6 @@
 import ramlab.display as display
 import ramlab.fit as fit
 import ramlab.hitran as hitran
-import ramlab.linelist as linelist
 import ramlab.molecules as molecules
 import ramlab.raman as raman
 import ramlab.simulate as simulate
